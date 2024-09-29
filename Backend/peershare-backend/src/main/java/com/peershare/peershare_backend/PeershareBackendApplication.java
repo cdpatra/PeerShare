e3c9e9609a5da3a37cb5568977bc3172cb07c001
cdpatra <cdpatra03@gmail.com>
2024-09-29 13:26:58 +0530

    start the spring boot maven project and add .gitignore file

---
+++ Backend/peershare-backend/src/main/java/com/peershare/peershare_backend/PeershareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.peershare.peershare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeershareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeershareBackendApplication.class, args);
+	}
+
+}
